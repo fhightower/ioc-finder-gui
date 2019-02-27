e4394049f2deb7639b408d9bd8888ff837f70002
Floyd Hightower <floyd.hightower27@gmail.com>
2019-02-27 06:36:13 -0400

    Formatting code

--- tests/test_ioc_finder_gui.py
+++ tests/test_ioc_finder_gui.py
@@ -7,7 +7,6 @@ from ioc_finder_gui import ioc_finder_gui
 
 
 class IocFinderGuiTestCase(unittest.TestCase):
-
     def setUp(self):
         self.app = ioc_finder_gui.app.test_client()
 
@@ -17,8 +16,6 @@ class IocFinderGuiTestCase(unittest.TestCase):
         self.assertIn('GUI for the indicator of compromise project.', rv.data.decode())
 
     def test_ioc_find_1(self):
-        rv = self.app.post('/find', data={
-            'text': 'foo example.com bar'
-        })
+        rv = self.app.post('/find', data={'text': 'foo example.com bar'})
         assert rv.status_code == 200
         assert 'example.com' in rv.data.decode()
